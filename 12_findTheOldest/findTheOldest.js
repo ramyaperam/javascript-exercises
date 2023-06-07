483ed6043b6fcd567114f44f0d7aafdd1b833df2
ramyaperam <peram.ramya77@gmail.com>
2023-06-07 12:34:29 -0500

    this is pro 12 by ramya

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    
+    // Calculate the age for each person
+    const ages = people.map(person => {
+      if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+      } else {
+        return currentYear - person.yearOfBirth;
+      }
+    });
+    
+    // Find the index of the oldest person
+    const oldestIndex = ages.indexOf(Math.max(...ages));
+    
+    // Return the oldest person
+    return people[oldestIndex];
+  };
+  
+  module.exports = findTheOldest;
+  
