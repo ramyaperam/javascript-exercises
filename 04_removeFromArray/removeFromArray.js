9941c2185584c9d2a772e0b1a492d4612cf9d02b
ramyaperam <peram.ramya77@gmail.com>
2023-06-07 10:57:58 -0500

    This is progfour by ramya

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,5 @@
 const removeFromArray = function (array, ...elementsToRemove) {
-    // Use Array.filter() to create a new array excluding the elements to be removed
     return array.filter((element) => !elementsToRemove.includes(element));
   };
-  
   module.exports = removeFromArray;
   
