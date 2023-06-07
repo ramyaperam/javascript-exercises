3471bfe8a03f2555d7cc801b57cc0815277211d0
ramyaperam <peram.ramya77@gmail.com>
2023-06-07 11:02:21 -0500

    this is leapyear by ramya

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+const leapYears = function(year) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  module.exports = leapYears;
+  
