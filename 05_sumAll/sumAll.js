0e66d1dde20c6586a92ad612822ab6092bc34f97
ramyaperam <peram.ramya77@gmail.com>
2023-06-07 10:59:22 -0500

    this is progfive by ramya

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,8 @@
 const sumAll = function(start, end) {
-    // Swap start and end values if necessary
     if (start > end) {
       [start, end] = [end, start];
     }
-  
     let sum = 0;
-  
-    // Iterate over the range and accumulate the sum
     for (let i = start; i <= end; i++) {
       sum += i;
     }
