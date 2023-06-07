fa66d88c7afb4959189a70310e28d9205ebd70b4
ramyaperam <peram.ramya77@gmail.com>
2023-06-07 11:32:44 -0500

    this is progseven by ramya

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(start, end) {
-    if (start > end) {
-      [start, end] = [end, start];
-    }
+    // if (start > end) {
+    //   [start, end] = [end, start];
+    // }
     let sum = 0;
     for (let i = start; i <= end; i++) {
       sum += i;
